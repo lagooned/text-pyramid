ca305d303af13cd35b9404230e2c2b29851b0515
Jared Engler <jared.lite@gmail.com>
2020-08-02 04:18:31 -0500

    make it work in general

--- src/test/java/com/jaredengler/AppTest.java
+++ src/test/java/com/jaredengler/AppTest.java
@@ -2,6 +2,7 @@
 package com.jaredengler;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,17 +18,44 @@ public class AppTest {
 
     @Test
     public void testZeroHeight() {
-        assertEquals(">", textPyramid.generate(0, '-'));
+        var text = textPyramid.generate(0, "-");
+        assertEquals(">", text);
     }
 
     @Test
     public void testOneHeight() {
-        assertEquals("->", textPyramid.generate(1, '-'));
+        assertEquals("->", textPyramid.generate(1, "-"));
     }
 
     @Test
     public void testTwoHeight() {
-        assertEquals("-\n-->\n-", textPyramid.generate(2, '-'));
+        assertEquals("-\n-->\n-", textPyramid.generate(2, "-"));
+    }
+
+    @Test
+    public void testThreeHeight() {
+        assertEquals("-\n--\n--->\n--\n-", textPyramid.generate(3, "-"));
+    }
+
+    @Test
+    public void testNHeight() {
+
+        for (int i = 4; i < 100; i++) {
+
+            String pyramidString = textPyramid.generate(i, "-");
+            assertTrue(pyramidString.contains("-".repeat(i).concat(">")));
+
+            int lowerBound = i * i / 2;
+            int upperBound = 2 * (lowerBound + i);
+            int pyramidStringLength = pyramidString.length();
+            boolean lowerBoundCheck = pyramidStringLength >= lowerBound;
+            Boolean upperBoundCheck = upperBound >= pyramidStringLength;
+
+            assertTrue(lowerBoundCheck);
+            assertTrue(upperBoundCheck);
+
+        }
+
     }
 
 }
--- src/test/java/com/jaredengler/TextPyramid.java
+++ src/test/java/com/jaredengler/TextPyramid.java
@@ -1,12 +1,20 @@
 package com.jaredengler;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class TextPyramid {
 
-    String generate(Integer height, Character c) {
-        if (height < 1) {
-            return ">";
-        }
-        return "->";
+    String generate(Integer height, String c) {
+        return IntStream.iterate(-height, i -> i + 1).boxed().limit(2 * height + 1).map(n -> {
+            if (n > 0)
+                return -n;
+            return n;
+        }).map(n -> n + height).map(n -> {
+            if (n == height)
+                return c.repeat(n).concat(">");
+            return c.repeat(n);
+        }).collect(Collectors.joining("\n")).trim();
     }
 
 }
